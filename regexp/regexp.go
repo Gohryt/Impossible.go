544f3eef84b5fba51f57fca8bd086feed80aa2c3
gohryt <kosachev_alex@mail.ru>
2021-01-12 13:13:04 +0300

    2021.01.26

---
+++ regexp/regexp.go
@@ -0,0 +1,42 @@
+package regexp
+
+import (
+	gjson "github.com/Gohryt/Impossible.go/json"
+	"regexp"
+)
+
+type (
+	Expression struct {
+		Compiled *regexp.Regexp
+		Real     struct {
+			String string
+			With   string
+		}
+	}
+)
+
+func (r *Expression) Replace(target *string) {
+	*target = r.Compiled.ReplaceAllString(*target, r.Real.With)
+}
+
+func (r *Expression) FromMemory(data []byte, errorHandler func(*error)) {
+	gjson.FromMemory(data, &r.Real, errorHandler)
+	r.Compiled = regexp.MustCompile(r.Real.String)
+	return
+}
+
+func (r *Expression) FromFile(filePath string, errorHandler func(*error)) {
+	gjson.FromFile(filePath, &r.Real, errorHandler)
+	r.Compiled = regexp.MustCompile(r.Real.String)
+	return
+}
+
+func (r *Expression) ToMemory(errorHandler func(*error)) (data []byte) {
+	data = gjson.ToMemory(r.Real, errorHandler)
+	return
+}
+
+func (r *Expression) ToFile(filePath string, errorHandler func(*error)) {
+	gjson.ToFile(r.Real, filePath, errorHandler)
+	return
+}
